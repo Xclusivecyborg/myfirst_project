69de1f9c43ea4eadc1257e6d8819d252cd590ec6
Xclusivecyborg <xclusivecyborg@gmail.com>
2022-06-08 09:54:32 +0100

    commit

--- home_project/profileview/views.py
+++ home_project/profileview/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 
 def aboutMe(request):
-    return HttpResponse("<h1> Hello, welcome to my profile. I am Ayodeji Ogundairo a full stack mobile engineer\n I build mobile apps with flutter and I am currently learning python(django) at kode camp</h1> ")  
+    return render(request, 'profileview/profile.html')
 
 def contactMe(request):
-    return HttpResponse("<h1> You can reach me on phone via +2348030861111</h1> ")
+    return render(request, 'profileview/contact.html')
 
 def myProjects(request):
-    return HttpResponse("<h1> I just finished building a crypto trading app for a fintech startup.</h1> \n <p><a href='https://docs.google.com/document/d/1B2mxS2yRezJGUvSQGIeyLPMWeeOCYPW1/edit'>Check out my resume here!</a></p>")
+    return render(request, 'profileview/projects.html')
  
